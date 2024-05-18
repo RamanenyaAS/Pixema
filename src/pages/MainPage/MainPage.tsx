a9f33d9c3356182d41e25231741373b6881aeb62
Alexey <riprop575@gmail.com>
2024-05-19 01:22:47 +0300

    feat(Pixema): add aside in main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,32 @@
+import "./MainPage.css"
 import IconHome from "../../images/IconHome.svg"
 import IconSetting from "../../images/IconSetting.svg"
 import IconTrends from "../../images/IconTrends.svg"
 import IconFavorites from "../../images/IconFavorites.svg"
 import IconLogo from "../../images/Logo.svg"
+import Link from "../../components/Link/Link"
 
 function MainPage() {
-  return ( 
+  return (
     <>
       <div className="container">
-        <img className="logo" src={IconLogo} alt="Pixema logo" />
+        <aside className="aside">
+          <img className="logo" src={IconLogo} alt="Pixema logo" />
+          <Link title="Home" src={IconHome} isDisabled={false}></Link>
+          <Link title="Trends" src={IconTrends} isDisabled={false}></Link>
+          <Link title="Favorites" src={IconFavorites} isDisabled={false}></Link>
+          <Link title="Setting" src={IconSetting} isDisabled={false}></Link>
+          <footer>
+            <div className="copyright">© All Rights Reserved</div>
+          </footer>
+        </aside>
+        <div className="main-page-block">
+
+        </div>
+
       </div>
     </>
-   );
+  );
 }
 
 export default MainPage;
