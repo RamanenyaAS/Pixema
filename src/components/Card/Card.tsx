8ece9f23c0a1de859af12128c794306759c20d08
Alexey <riprop575@gmail.com>
2024-06-25 23:45:58 +0300

    feat(Pixema): add new API request for dynamic data in Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,54 @@
 import "./Card.css"
 import { Link } from "react-router-dom"
 import { IMovie } from "../../types/interfaces";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../providers/myContext";
 
+
+
+const movieKey = "a705d9d9";
+
+async function fetchDetails(id: string): Promise<{ Genre: string, imdbRating: string }> {
+  try {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${movieKey}&i=${id}`);
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const data = await response.json();
+    return {
+      Genre: data.Genre,
+      imdbRating: data.imdbRating
+    };
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+}
 function Card({ movie }: { movie: IMovie }) {
-  const [topic]= useContext(ThemeContext);
-  const rating:any = movie.Ratings;
+  const [topic] = useContext(ThemeContext);
+  const [movieDetails, setMovieDetails] = useState<({ Genre: string, imdbRating: string } | null)>(null)
+  useEffect(() => {
+    fetchDetails(movie.imdbID)
+      .then((data) => {
+        setMovieDetails(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movie details:", error);
+      });
+  }, [movie.imdbID]);
+
   return (
     <>
       <div className="card">
         <Link className="link" key={movie.imdbID} to={`/movie/${movie.imdbID}`}>
-          <div className="card__rating">{rating}</div>
-          <img className="card__image" src={movie.Poster} alt="" />
-          <div className={`card__title card__title_${topic}`}>{movie.Title}</div>
-          <div className="card__genre">{movie.Genre}</div>
+          {movieDetails && (
+            <>
+              <div className="card__rating">{movieDetails.imdbRating}</div>
+              <img className="card__image" src={movie.Poster} alt="" />
+              <div className={`card__title card__title_${topic}`}>{movie.Title}</div>
+              <div className="card__genre">{movieDetails.Genre}</div>
+            </>
+          )}
         </Link>
       </div>
     </>
